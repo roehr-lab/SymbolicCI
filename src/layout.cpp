968c17155a4aba5b1a57a2fd954bff6221b96206
Anurag <singhanu@132.187.199.145>
2023-07-16 02:49:15 +0200

    function2

--- src/layout.cpp
+++ src/layout.cpp
@@ -3,25 +3,6 @@
 //  \file layout.cpp
 //  \brief Provides member functions definitions for the layout classes.
 //
-//  Copyright (c) 2022 Mohammed Maher Abdelrahim Mohammed
-//  
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//  
-//  The above copyright notice and this permission notice shall be included in all
-//  copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//  SOFTWARE.
 */
 //=================================================================================================
      
@@ -53,18 +34,6 @@ void message::Info(void)
 -------------------------------------------------------------------------------------------*/
 void message::Name(void)
 {
-	printf("\n");
-	printf("--▂▃▄▅▆▇█▓▒░Clebsch-Gordan v1.1.0-a.1░▒▓█▇▆▅▄▃▂ --\n\n");
-	printf("                 ,-----. ,---. ,--.   ,-----. \n");
-	printf(" ,---.,---. ,---'  .--.//  O  \\|  |  '  .--./ \n");
-	printf("| .--| .-. | .--|  |   |  .-.  |  |  |  |     \n");
-	printf("\\ `--' '-' \\ `--'  '--'|  | |  |  '--'  '--'\\ \n");
-	printf(" `---.`-  / `---'`-----`--' `--`-----'`-----' \n");
-	printf("     `---'                                    \n");
-	//printf("\n");
-	printf("Symbolic Clebsch-Gordan Coefficients Calculator \n\n");
-	printf("    MOHAMMED MAHER ABDELRAHIM MOHAMMED 2022  \n");
-	printf("          maherali8932@gmail.com             \n\n");
 	//printf("**********************************************\n");
 }
 
